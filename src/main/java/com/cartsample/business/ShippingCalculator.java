59a3c3e71a29577b04fc6eed23ee51c77ade1d0d
affanq <32521099+affanq@users.noreply.github.com>
2017-10-05 14:01:21 -0500

    Removed sys out statement

--- src/main/java/com/cartsample/business/ShippingCalculator.java
+++ src/main/java/com/cartsample/business/ShippingCalculator.java
@@ -6,7 +6,5 @@ public class ShippingCalculator implements CalculatorType {
 
     public void calculateCost(Cart cart) {
         cart.setCartTotal(cart.getCartTotal() + cart.getShippingType().getShippingCost());
-
-        System.out.println("Shipping Calculator - Shipping cost: " + cart.getShippingType().getShippingCost()+ ", Cart Total: " + cart.getCartTotal());
     }
 }
